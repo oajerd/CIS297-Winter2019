2133e250df3b8959297b27a5299562c80a051046
Eric Charnesky <eric.charnesky@careevolution.com>
2019-01-19 12:32:47 -0500

    Math Tutor

---
+++ MathTutor/MathTutor/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace MathTutor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNumberLabel = new System.Windows.Forms.Label();
+            this.secondNumberLabel = new System.Windows.Forms.Label();
+            this.operatorLabel = new System.Windows.Forms.Label();
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNumberLabel
+            // 
+            this.firstNumberLabel.AutoSize = true;
+            this.firstNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNumberLabel.Location = new System.Drawing.Point(67, 143);
+            this.firstNumberLabel.Name = "firstNumberLabel";
+            this.firstNumberLabel.Size = new System.Drawing.Size(0, 63);
+            this.firstNumberLabel.TabIndex = 0;
+            // 
+            // secondNumberLabel
+            // 
+            this.secondNumberLabel.AutoSize = true;
+            this.secondNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondNumberLabel.Location = new System.Drawing.Point(276, 143);
+            this.secondNumberLabel.Name = "secondNumberLabel";
+            this.secondNumberLabel.Size = new System.Drawing.Size(0, 63);
+            this.secondNumberLabel.TabIndex = 1;
+            // 
+            // operatorLabel
+            // 
+            this.operatorLabel.AutoSize = true;
+            this.operatorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operatorLabel.Location = new System.Drawing.Point(151, 143);
+            this.operatorLabel.Name = "operatorLabel";
+            this.operatorLabel.Size = new System.Drawing.Size(59, 63);
+            this.operatorLabel.TabIndex = 2;
+            this.operatorLabel.Text = "+";
+            // 
+            // answerBox
+            // 
+            this.answerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerBox.Location = new System.Drawing.Point(472, 138);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.Size = new System.Drawing.Size(100, 68);
+            this.answerBox.TabIndex = 3;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(651, 147);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(193, 59);
+            this.submitButton.TabIndex = 4;
+            this.submitButton.Text = "Check Answer";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(318, 40);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 46);
+            this.statusLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 513);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.operatorLabel);
+            this.Controls.Add(this.secondNumberLabel);
+            this.Controls.Add(this.firstNumberLabel);
+            this.Name = "Form1";
+            this.Text = "Math Tutor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstNumberLabel;
+        private System.Windows.Forms.Label secondNumberLabel;
+        private System.Windows.Forms.Label operatorLabel;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ MathTutor/MathTutor/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathTutor
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int firstNumber;
+        int secondNumber;
+        public Form1()
+        {
+            InitializeComponent();
+            PickNewNumbers();
+        }
+
+        private void PickNewNumbers()
+        {
+            firstNumber = random.Next(1, 11);
+            secondNumber = random.Next(1, 11);
+            firstNumberLabel.Text = firstNumber.ToString();
+            secondNumberLabel.Text = secondNumber.ToString();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            int answer = Convert.ToInt32(answerBox.Text);
+            if ( answer == firstNumber + secondNumber )
+            {
+                statusLabel.Text = "Correct!";
+                PickNewNumbers();
+            }
+            else
+            {
+                statusLabel.Text = "Try again!";
+            }
+            answerBox.Text = string.Empty;
+        }
+    }
+}
