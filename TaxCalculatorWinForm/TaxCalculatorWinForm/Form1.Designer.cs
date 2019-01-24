637cf9c5d48425e44bd927092884dbad2676029d
Eric Charnesky <eric.charnesky@careevolution.com>
2019-01-24 18:46:17 -0500

    TaxCalculatorWinForm

---
+++ TaxCalculatorWinForm/TaxCalculatorWinForm/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace TaxCalculatorWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.incomeTextBox = new System.Windows.Forms.TextBox();
+            this.addIncomeButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.deductionTextBox = new System.Windows.Forms.TextBox();
+            this.addDeductionButton = new System.Windows.Forms.Button();
+            this.grossIncomeLabel = new System.Windows.Forms.Label();
+            this.totalDeductionLabel = new System.Windows.Forms.Label();
+            this.taxInfoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Income:";
+            // 
+            // incomeTextBox
+            // 
+            this.incomeTextBox.Location = new System.Drawing.Point(175, 68);
+            this.incomeTextBox.Name = "incomeTextBox";
+            this.incomeTextBox.Size = new System.Drawing.Size(100, 31);
+            this.incomeTextBox.TabIndex = 1;
+            // 
+            // addIncomeButton
+            // 
+            this.addIncomeButton.Location = new System.Drawing.Point(292, 64);
+            this.addIncomeButton.Name = "addIncomeButton";
+            this.addIncomeButton.Size = new System.Drawing.Size(163, 38);
+            this.addIncomeButton.TabIndex = 2;
+            this.addIncomeButton.Text = "Add Income";
+            this.addIncomeButton.UseVisualStyleBackColor = true;
+            this.addIncomeButton.Click += new System.EventHandler(this.addIncomeButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dedution:";
+            // 
+            // deductionTextBox
+            // 
+            this.deductionTextBox.Location = new System.Drawing.Point(174, 135);
+            this.deductionTextBox.Name = "deductionTextBox";
+            this.deductionTextBox.Size = new System.Drawing.Size(100, 31);
+            this.deductionTextBox.TabIndex = 4;
+            // 
+            // addDeductionButton
+            // 
+            this.addDeductionButton.Location = new System.Drawing.Point(292, 135);
+            this.addDeductionButton.Name = "addDeductionButton";
+            this.addDeductionButton.Size = new System.Drawing.Size(175, 48);
+            this.addDeductionButton.TabIndex = 5;
+            this.addDeductionButton.Text = "Add Deduction";
+            this.addDeductionButton.UseVisualStyleBackColor = true;
+            this.addDeductionButton.Click += new System.EventHandler(this.addDeductionButton_Click);
+            // 
+            // grossIncomeLabel
+            // 
+            this.grossIncomeLabel.AutoSize = true;
+            this.grossIncomeLabel.Location = new System.Drawing.Point(550, 64);
+            this.grossIncomeLabel.Name = "grossIncomeLabel";
+            this.grossIncomeLabel.Size = new System.Drawing.Size(150, 25);
+            this.grossIncomeLabel.TabIndex = 6;
+            this.grossIncomeLabel.Text = "Gross Income:";
+            // 
+            // totalDeductionLabel
+            // 
+            this.totalDeductionLabel.AutoSize = true;
+            this.totalDeductionLabel.Location = new System.Drawing.Point(550, 141);
+            this.totalDeductionLabel.Name = "totalDeductionLabel";
+            this.totalDeductionLabel.Size = new System.Drawing.Size(186, 25);
+            this.totalDeductionLabel.TabIndex = 7;
+            this.totalDeductionLabel.Text = "Total Deductions: ";
+            // 
+            // taxInfoLabel
+            // 
+            this.taxInfoLabel.AutoSize = true;
+            this.taxInfoLabel.Location = new System.Drawing.Point(205, 298);
+            this.taxInfoLabel.Name = "taxInfoLabel";
+            this.taxInfoLabel.Size = new System.Drawing.Size(89, 25);
+            this.taxInfoLabel.TabIndex = 8;
+            this.taxInfoLabel.Text = "Tax Info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 689);
+            this.Controls.Add(this.taxInfoLabel);
+            this.Controls.Add(this.totalDeductionLabel);
+            this.Controls.Add(this.grossIncomeLabel);
+            this.Controls.Add(this.addDeductionButton);
+            this.Controls.Add(this.deductionTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.addIncomeButton);
+            this.Controls.Add(this.incomeTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox incomeTextBox;
+        private System.Windows.Forms.Button addIncomeButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox deductionTextBox;
+        private System.Windows.Forms.Button addDeductionButton;
+        private System.Windows.Forms.Label grossIncomeLabel;
+        private System.Windows.Forms.Label totalDeductionLabel;
+        private System.Windows.Forms.Label taxInfoLabel;
+    }
+}
+
---
+++ TaxCalculatorWinForm/TaxCalculatorWinForm/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCalculatorWinForm
+{
+    public partial class Form1 : Form
+    {
+        TaxesFor2018 taxInfo;
+        double grossIncome;
+        double totalDeductions;
+        public Form1()
+        {
+            InitializeComponent();
+            grossIncome = 0;
+            totalDeductions = 0;
+            UpdateTaxInfo();
+        }
+
+        private void addIncomeButton_Click(object sender, EventArgs e)
+        {
+            grossIncome += Convert.ToDouble(incomeTextBox.Text);
+            grossIncomeLabel.Text = $"Gross Income: ${grossIncome}";
+            incomeTextBox.Text = string.Empty;
+            UpdateTaxInfo();
+        }
+
+        private void addDeductionButton_Click(object sender, EventArgs e)
+        {
+            totalDeductions += Convert.ToDouble(deductionTextBox.Text);
+            totalDeductionLabel.Text = $"Total Deductions: ${totalDeductions}";
+            deductionTextBox.Text = string.Empty;
+            UpdateTaxInfo();
+        }
+
+        public void UpdateTaxInfo()
+        {
+            taxInfo = new TaxesFor2018(grossIncome, totalDeductions);
+
+            taxInfoLabel.Text = $"Gross Income: {grossIncome}"
+           + "\n" + $"Total Deductions: {taxInfo.TotalDeductions}"
+           + "\n" + $"Taxes owed at 10%: {taxInfo.Taxes_owed_at_10_percent}"
+           + "\n" + $"Taxes owed at 12%: {taxInfo.Taxes_owed_at_12_percent}"
+           + "\n" + $"Taxes owed at 22%: {taxInfo.Taxes_owed_at_22_percent}"
+           + "\n" + $"Taxes owed at 24%: {taxInfo.Taxes_owed_at_24_percent}"
+           + "\n" + $"Taxes owed at 32%: {taxInfo.Taxes_owed_at_32_percent}"
+           + "\n" + $"Taxes owed at 35%: {taxInfo.Taxes_owed_at_35_percent}"
+           + "\n" + $"Taxes owed at 37%: {taxInfo.Taxes_owed_at_37_percent}"
+           + "\n" + $"Taxes owed at a percentage of Adjusted Gross Income: {taxInfo.TaxesOwedAsPercentageOfAdjustedGrossIncome}"
+           + "\n" + $"Taxes owed at a percentage of Gross Income: {taxInfo.TaxesOwedAsPercentageOfGrossIncome}";
+
+        }
+
+    }
+
+    public class TaxesFor2018
+    {
+        static int[] Tax_Bracket_Floors = { 600_000, 400_000, 315_000, 165_000, 77_400, 19_050, 0 };
+        static double[] Tax_Bracket_Rates = { .37, .35, .32, .24, .22, .12, .1 };
+
+        public double Taxes_owed_at_37_percent { get; private set; }
+        public double Taxes_owed_at_35_percent { get; private set; }
+        public double Taxes_owed_at_32_percent { get; private set; }
+        public double Taxes_owed_at_24_percent { get; private set; }
+        public double Taxes_owed_at_22_percent { get; private set; }
+        public double Taxes_owed_at_12_percent { get; private set; }
+        public double Taxes_owed_at_10_percent { get; private set; }
+
+        public double TotalTaxesOwed { get; private set; }
+
+        public double TaxesOwedAsPercentageOfGrossIncome { get; private set; }
+        public double TaxesOwedAsPercentageOfAdjustedGrossIncome { get; private set; }
+
+        private double GrossIncome;
+        public double TotalDeductions { get; private set; }
+        private double AdjustedGrossIncome;
+
+        public TaxesFor2018(double GrossIncome, double TotalDeductions)
+        {
+            this.GrossIncome = GrossIncome;
+            if (TotalDeductions < 24_000)
+            {
+                TotalDeductions = 24_000;
+            }
+            this.TotalDeductions = TotalDeductions;
+            AdjustedGrossIncome = GrossIncome - TotalDeductions;
+            double incomeToBeTaxed = AdjustedGrossIncome;
+            TotalTaxesOwed = 0;
+
+            double[] taxes_at_brackets = { 0, 0, 0, 0, 0, 0, 0 };
+
+            for (int index = 0; index < Tax_Bracket_Floors.Length; index++)
+            {
+                if (incomeToBeTaxed > Tax_Bracket_Floors[index])
+                {
+                    taxes_at_brackets[index] = (incomeToBeTaxed - Tax_Bracket_Floors[index]) * Tax_Bracket_Rates[index];
+                    incomeToBeTaxed = Tax_Bracket_Floors[index];
+                    TotalTaxesOwed += taxes_at_brackets[index];
+                }
+            }
+
+            Taxes_owed_at_37_percent = taxes_at_brackets[0];
+            Taxes_owed_at_35_percent = taxes_at_brackets[1];
+            Taxes_owed_at_32_percent = taxes_at_brackets[2];
+            Taxes_owed_at_24_percent = taxes_at_brackets[3];
+            Taxes_owed_at_22_percent = taxes_at_brackets[4];
+            Taxes_owed_at_12_percent = taxes_at_brackets[5];
+            Taxes_owed_at_10_percent = taxes_at_brackets[6];
+
+            TaxesOwedAsPercentageOfGrossIncome = TotalTaxesOwed / GrossIncome;
+            TaxesOwedAsPercentageOfAdjustedGrossIncome = TotalTaxesOwed / AdjustedGrossIncome;
+
+        }
+
+
+
+
+
+    }
+
+}
